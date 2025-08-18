e12c6e2d5f6f88824703faab4d8d138f36509e8c
TheodorDM <st129282@studem.spbu.ru>
2025-08-18 11:45:08 +0300

    fix: GaussFiltr now works correct

--- GaussFiltr.cpp
+++ GaussFiltr.cpp
@@ -10,11 +10,9 @@ void BMP::GaussFiltr(float kernelSize)
 {
     float sigma = (bmp_info_header.width + bmp_info_header.height - 2 * kernelSize)/4;
     
-    
     std::vector<std::vector<float>> ratio(kernelSize, std::vector<float>(kernelSize));
     double sum = 0.0;
     
-
     for (int x = 0; x < kernelSize; x++)
     {
         for (int y = 0; y < kernelSize; y++)
@@ -25,9 +23,8 @@ void BMP::GaussFiltr(float kernelSize)
             float GaussFun = (1 / (2 * M_PI * pow(sigma, 2))) * std::exp(-( pow(mutX, 2) + pow(mutY, 2)) / (2 * pow(sigma, 2)));
             ratio[x][y]= GaussFun;
             sum += GaussFun;
-        
         }
-    };
+    }
     for (int x = 0; x < kernelSize; x++)
     {
         for (int y = 0; y < kernelSize; y++)
@@ -35,29 +32,33 @@ void BMP::GaussFiltr(float kernelSize)
             ratio[x][y] /= sum;
         }
     }
-    std::vector<uint8_t> new_data(data.size());
+    std::vector<uint8_t> new_data(data); // Инициализация копией исходных данных
 
-    for (int i = kernelSize / 2; i < bmp_info_header.width - kernelSize / 2; i++)
+    for (int i = kernelSize / 2; i <= bmp_info_header.width - kernelSize / 2; i++)
     {
-        for (int j = kernelSize / 2; j < bmp_info_header.height - kernelSize / 2; j++)
+        for (int j = kernelSize / 2; j <= bmp_info_header.height - kernelSize / 2; j++)
         {
-            float redSum = 0, greenSum = 0, blueSum = 0;
+            float blueSum = 0, greenSum = 0, redSum = 0;
             for (int k = 0; k < kernelSize; k++)
             {
                 for (int l = 0; l < kernelSize; l++)
                 {
-                    int pixelIndex = ((j + k - kernelSize / 2) * bmp_info_header.width + (i + k - kernelSize / 2)) * 3;
-                    redSum += data[pixelIndex] * ratio[k][l];
+                    // Исправление индексации: k - вертикаль, l - горизонталь
+                    int x_index = i + l - kernelSize / 2;
+                    int y_index = j + k - kernelSize / 2;
+                    int pixelIndex = (y_index * bmp_info_header.width + x_index) * 3;
+                    
+                    blueSum  += data[pixelIndex]     * ratio[k][l];
                     greenSum += data[pixelIndex + 1] * ratio[k][l];
-                    blueSum += data[pixelIndex + 2] * ratio[k][l];
+                    redSum   += data[pixelIndex + 2] * ratio[k][l];
                 }
-            };
+            }
             int newIndex = (j * bmp_info_header.width + i) * 3;
-            new_data[newIndex] = std::min(255, std::max(0, static_cast<int>(redSum)));
+            new_data[newIndex]     = std::min(255, std::max(0, static_cast<int>(blueSum)));
             new_data[newIndex + 1] = std::min(255, std::max(0, static_cast<int>(greenSum)));
-            new_data[newIndex + 2] = std::min(255, std::max(0, static_cast<int>(blueSum)));
+            new_data[newIndex + 2] = std::min(255, std::max(0, static_cast<int>(redSum)));
         }
     }
 
     data.swap(new_data);
-};
+}
--- main.cpp
+++ main.cpp
@@ -22,9 +22,9 @@ int main()
     
         BMP bmp("source.bmp");
         bmp.clockwiseRotate();
-        //parametr must not be odd
+        //parametr must be odd
         //if its nessecary to make blur stronger, you may make perameter bigger
-        bmp.GaussFiltr(8);
+        bmp.GaussFiltr(39);
         bmp.write("GFiltr.bmp");
         
     }
